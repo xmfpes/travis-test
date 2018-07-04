fa8e0eb45f0b434f9fecc1a91c8b2f15fe1ec454
우아테크캠프_22 <woowatechcamp_22@woowatechcamp22.local>
2018-07-04 16:46:57 +0900

    test

--- src/main/java/com/travis/test/TestApplication.java
+++ src/main/java/com/travis/test/TestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestApplication {
 
     public static void main(String[] args) {
+        System.out.print("Hello");
         SpringApplication.run(TestApplication.class, args);
     }
 }
